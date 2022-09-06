cb5f6f621cc34771cad80279bd398ff416a17184
apsy2003 <apsy2003@naver.com>
2022-09-06 12:02:14 +0900

    myJSP

---
+++ pro05/src/sec01/ex01/ThirdServlet.java
@@ -0,0 +1,28 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*(에너테이션:프로그램의 소스코드 안에 다른 프로그램을 위한 정보를 미리 약속된 형식으로 포함시킨 것.)*/
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ThirdServlet init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("ThirdServlet destroy 메서드 호출");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet doGet 메서드 호출");
+	}
+
+}
